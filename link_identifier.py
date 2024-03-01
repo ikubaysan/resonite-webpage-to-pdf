e8646f6d6ec7039148d36ec78e767e2741bda966
Ikubay <ikubaysan@gmail.com>
2024-02-29 18:45:21 -0800

    update link identifier

--- link_identifier.py
+++ link_identifier.py
@@ -5,9 +5,12 @@ class Link:
     def __init__(self, uri: str, bounds: Tuple[float, float, float, float]):
         self.uri = uri
         self.bounds = bounds  # (x0, y0, x1, y1)
+        self.bounds_width = bounds[2] - bounds[0]
+        self.bounds_height = bounds[3] - bounds[1]
+        self.origin = (bounds[0], bounds[1])
 
     def __repr__(self):
-        return f"Link(uri={self.uri}, bounds={self.bounds})"
+        return f"Link(uri={self.uri}, bounds={self.bounds}) bounds_width={self.bounds_width}, bounds_height={self.bounds_height}, origin={self.origin}"
 
 class Page:
     def __init__(self, number: int, size: Tuple[float, float]):
@@ -34,6 +37,9 @@ class Document:
             page_links = page.get_links()
             for link in page_links:
                 if link['kind'] == fitz.LINK_URI:
+                    # Ignore non-http and non-https links
+                    if not link['uri'].startswith('http'):
+                        continue
                     link_obj = Link(link['uri'], (link['from'].x0, link['from'].y0, link['from'].x1, link['from'].y1))
                     page_obj.add_link(link_obj)
             self.pages.append(page_obj)
@@ -42,7 +48,29 @@ class Document:
     def __repr__(self):
         return f"Document(path={self.path}, pages={self.pages})"
 
+
+    def get_resonite_string(self) -> str:
+        resonite_string = ""
+        for page in self.pages:
+            resonite_string += ">" # Indicate start of a new page
+            for link in page.links:
+                resonite_string += "<" # Indicate start of a link object
+                # Write link's x origin, delimited with a pipe
+                resonite_string += f"{link.origin[0]}|"
+                # Write link's y origin, delimited with a pipe
+                resonite_string += f"{link.origin[1]}|"
+                # Write link's width, delimited with a pipe
+                resonite_string += f"{link.bounds_width}|"
+                # Write link's height, delimited with a pipe
+                resonite_string += f"{link.bounds_height}|"
+                # Write link's URI
+                resonite_string += f"{link.uri}"
+
+        return resonite_string
+
 # Example usage
 pdf_path = 'asdf.pdf'
+#pdf_path = 'resowiki.pdf'
 document = Document(pdf_path)
 print(document)
+print(document.get_resonite_string())
