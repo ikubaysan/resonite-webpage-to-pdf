5624b54561a2d3e48bf9b4b7f01513fc97fc53ea
Ikubay <ikubaysan@gmail.com>
2024-03-04 00:24:35 -0800

    selenium_test works with ebay

--- selenium_test.py
+++ selenium_test.py
@@ -6,18 +6,19 @@ options = ChromeOptions()
 
 # Implicit wait
 options.add_argument("--wait-for-browser")
+options.add_argument("--headless")
 
 # Initialize undetected Chrome driver
 driver = uc.Chrome(options=options)
 
-# URL launch
-driver.get("https://www.github.com/1arsitdenmauyarst")
+# URL launchS
+driver.get("https://www.ebay.com/arsdawfpgawfp")
 
 # Get current URL
 current_url = driver.current_url
 
 # Find the status code using JavaScript in the browser
-response_code = driver.execute_script('return fetch(arguments[0], {method: "HEAD"}).then(response => response.status);', current_url)
+response_code = driver.execute_script('return fetch(arguments[0], {method: "GET"}).then(response => response.status);', current_url)
 
 # Print response code
 print("Http response code:", response_code)
